bc97162673f06712831796b0c4ffeac273b0a205
Matheus Lima <eclicknoplay@gmail.com>
2022-12-18 10:29:42 -0400

    ✨ 💄 Change `NewTODO` and add `List` component

--- src/components/pages/Home/content.tsx
+++ src/components/pages/Home/content.tsx
@@ -1,18 +1,33 @@
-import { Box, Center, Checkbox, Input, Text } from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { Box, Button, Center, Checkbox, Divider, Input, Text } from "@chakra-ui/react";
 
 export function NewTODO ({
 
 } ){
     return (
         <>
-            <Box>
+            <Box display={'flex'} flexDirection={'column'}>
                 <Center>
-                    <Box w={'75%'} minH={'50px'} border={'2px'} borderColor={'black'} display={'flex'} flexDirection={'row'}>
-                        <Checkbox ml={'15px'}></Checkbox>
-                        <Input ml={'10px'} fontSize={'20px'} variant={'unstyled'} placeholder={'Insert a new to-do'}></Input>
-                    </Box>
-                </Center>
+                    <Box w={'75%'} minH={'50px'} boxShadow={'dark-lg'} display={'flex'} flexDirection={'row'}>
+                        <Input ml={'10px'} fontSize={'20px'} variant={'unstyled'} placeholder={'Insert a new to-do'}></Input>                        
+                        <Button mt={'5px'} mr={'10px'} w={'25%'} type={'submit'} rightIcon={<ArrowForwardIcon />}>Submit</Button>
+                    </Box>                                                            
+                </Center>                
             </Box>
+        </>
+    )
+}
+
+export function List ({
+
+} ){
+    return (
+        <>
+            <Center>
+                <Box mt={'50px'} w={'75%'} h={'500px'} overflowY={'auto'} boxShadow={'2xl'}>
+                    
+                </Box>
+            </Center>
         </>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Box, Center, Text } from '@chakra-ui/react'
 import Head from 'next/head'
 import { useState } from 'react'
 import styles from '../../styles/Home.module.css'
-import { NewTODO } from '../components/pages/Home'
+import { List, NewTODO } from '../components/pages/Home'
 import { MainLayout } from '../layout/MainLayout'
 
 export default function Home() {  
@@ -24,13 +24,17 @@ export default function Home() {
             minH={{base: '500px', lg: '700px'}}
             mt={'100px'}
             mb={'100px'}
-            boxShadow={'2xl'}
+            // boxShadow={'2xl'}
+            border='1px' //just debug
             display={'flex'}
             flexDirection={'column'}
           >
             <Text fontSize={'36px'} textAlign={'center'}>TO-DO List</Text>
             
             <NewTODO></NewTODO>
+
+            <List></List>
+
           </Box>
         </Center>
       </MainLayout>
